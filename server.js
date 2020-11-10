93c00b493afccbff1c5f6ec8c81fd665e1ba6e2b
Glitch (haru-tutorial) <none>
2020-11-10 08:36:53 +0000

    👮☕️ Updated with Glitch

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.token);
+
+client.on('ready', () => {
+  console.log("The bot is now online!")
+})
+
+const prefix = "!";
+client.on('message', (message) => {
+  
+})
+
+
+////////////////////////////////////////////////////////////////
 const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send(require('/app/html.js'))
+  res.send(require('/app/htmls/boxGame.js'))
 })
 
 app.listen(process.env.PORT)
+////////////////////////////////////////////////////////////////
