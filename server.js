6f79c7eaadbe49331aa5e6308095693c6919b100
NightCry1 <66012697+NightCry1@users.noreply.github.com>
2020-11-13 01:33:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
+  res.send('Add a "boxGame" or a "lolGen" to the end of this link to play a game!')
+})
+
+app.get('/', (req, res) => {
   res.send(require('/app/htmls/boxGame.js'))
+})
+
+app.get('/', (req, res) => {
+  res.send(require('/app/htmls/lolGen.js'))
 })
 
 app.listen(process.env.PORT)
