78f2babee0c2727cfc9cb0132bea5d6f76ac1dd0
NightCry1 <66012697+NightCry1@users.noreply.github.com>
2020-11-13 22:45:07 +0800

    Update lolGen.js

--- htmls/lolGen.js
+++ htmls/lolGen.js
@@ -1,6 +1,10 @@
 module.exports = `<html>
 	<head>
 		<title>LOL</title>
+		<link rel="SHORTCUT ICON" href="https://media.discordapp.net/attachments/708740034429190164/776819824067346482/download.jpg">
+    		<meta property="og:title" content="LOL">
+    		<meta property="og:description" content="It's not even a game. It's just a screensaver that spams lol around ur screen.">
+    		<meta property="og:image" content="https://media.discordapp.net/attachments/708740034429190164/776819824067346482/download.jpg">
 	</head>
 	<body>
 		<canvas id="canvas" height="620" width="1345">No canvas lol</canvas>
@@ -9,14 +13,14 @@ module.exports = `<html>
 			ctx.stroke()
 			ctx.fillStyle = "#ff00ff"
 			ctx.font = "50px sans-serif"
-			setTimeout(() => newLol(), 20)
+			setTimeout(() => newLol(), 200)
 	
 			
 			function newLol() {
 				let x = Math.floor(Math.random() * 1360)
 				let y = Math.floor(Math.random() * 750)
 				ctx.fillText("lol", x, y)
-				setTimeout(() => newLol(), 20)
+				setTimeout(() => newLol(), 200)
 			}
 		</script>
 	</body>
